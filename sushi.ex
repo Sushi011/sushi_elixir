ec20bd64c7e92d64ead4197fe68de1991aba67d4
sushmita <gitsushmita@gmail.com>
2023-02-04 19:11:15 +0800

    added new functions addition and multiplication

--- sushi.ex
+++ sushi.ex
@@ -3,4 +3,12 @@ defmodule Sushi do
   def hello() do
     IO.puts("Hello World")
   end
+
+  def multiplication_of_two_numbers(number1, number2) do
+    number1 * number2
+  end
+
+  def addition_of_two_nummbers(num1, num2) do
+    num1 + num2
+  end
 end
